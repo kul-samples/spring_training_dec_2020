dae2b9bc0021c1cb27a12637f55f2e0c28f0f87d
vatsank <vatsank@gmail.com>
2020-12-05 13:54:19 +0530

    Update

--- hospital-info-service/src/main/java/com/example/demo/model/Doctor.java
+++ hospital-info-service/src/main/java/com/example/demo/model/Doctor.java
@@ -1,5 +1,24 @@
 package com.example.demo.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Doctor {
 
+	int doctorId;
+	String doctorName;
+	
+	@Autowired
+	Patient patient;
+	
+	
 }
