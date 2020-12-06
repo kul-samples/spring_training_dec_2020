014e2be55b4c2af373b8002b518c43ed81fb8ca7
vatsank <vatsank@gmail.com>
2020-12-06 10:45:37 +0530

    Update

--- hospital-info-client/src/main/java/com/example/demo/controllers/HospitalClientController.java
+++ hospital-info-client/src/main/java/com/example/demo/controllers/HospitalClientController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.services.HospitalClientService;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class HospitalClientController {
 
+	
+	@Autowired
+	private HospitalClientService service;
+	
+	
+	@GetMapping(path = "/api/v1/client/patients")
+	public String getPatients() {
+		
+		return this.service.getHospitalObject();
+	}
 }
--- hospital-info-client/src/main/java/com/example/demo/services/HospitalClientService.java
+++ hospital-info-client/src/main/java/com/example/demo/services/HospitalClientService.java
@@ -1,5 +1,23 @@
 package com.example.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class HospitalClientService {
 
+	
+	private String baseURL = "http://localhost:6060/api/v1";
+	
+	@Autowired
+	private RestTemplate template;
+	
+	
+	public String getHospitalObject() {
+		
+		String reqURL= baseURL+"/patients";
+		
+	   return template.getForObject(reqURL, String.class);	
+	}
 }
