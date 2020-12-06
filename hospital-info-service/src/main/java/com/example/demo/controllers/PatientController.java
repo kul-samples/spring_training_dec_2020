6641a77f9574b742bc9f5b0c2c2cc13a215a1e26
vatsank <vatsank@gmail.com>
2020-12-06 09:48:31 +0530

    Update

--- hospital-info-service/src/main/java/com/example/demo/controllers/PatientController.java
+++ hospital-info-service/src/main/java/com/example/demo/controllers/PatientController.java
@@ -3,8 +3,17 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Patient;
@@ -24,4 +33,33 @@ public class PatientController {
 		
 		return this.service.getAllPatients();
 	}
+	
+	@GetMapping(path = "/api/v1/patients/{id}")
+	public Patient getPatient(@PathVariable("id") int id){
+		
+		return this.service.getPatientByid(id);
+	}
+	
+	@PostMapping(path = "/api/v1/patients")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Patient addPatient(@RequestBody Patient entity){
+		
+		return this.service.addPatient(entity);
+	}
+	
+	
+	@PutMapping(path = "/api/v1/patients")
+	public Patient updatePatient(@RequestBody Patient entity){
+		
+		return this.service.updatePatient(entity);
+	}
+	
+	
+	@DeleteMapping(path = "/api/v1/patients")
+	public ResponseEntity<String> remoPatient(@RequestBody Patient entity){
+		
+		   return ResponseEntity.ok("Record Deleted");
+		   
+	}
+	
 }
