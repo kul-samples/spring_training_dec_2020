5c3ea918d046736fe35a08890243444a149d42ca
vatsank <vatsank@gmail.com>
2020-12-06 15:54:59 +0530

    Update

--- path-labs-info-service/src/main/java/com/example/demo/repo/PathLabRepository.java
+++ path-labs-info-service/src/main/java/com/example/demo/repo/PathLabRepository.java
@@ -1,11 +1,16 @@
 package com.example.demo.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.demo.entity.PathLab;
 
 @RepositoryRestResource(path = "labs")
 public interface PathLabRepository extends JpaRepository<PathLab, Integer> {
 
+	@RestResource(path = "location")
+	public List<PathLab> findByLocation(String location);
 }
