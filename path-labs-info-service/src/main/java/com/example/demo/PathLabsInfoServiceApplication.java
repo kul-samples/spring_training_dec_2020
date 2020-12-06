b7b8e62661f594b468a2adb59bf94459068b8a72
vatsank <vatsank@gmail.com>
2020-12-06 15:47:01 +0530

    Update

--- path-labs-info-service/src/main/java/com/example/demo/PathLabsInfoServiceApplication.java
+++ path-labs-info-service/src/main/java/com/example/demo/PathLabsInfoServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.example.demo.repo.PathLabRepository;
 import com.example.demo.entity.*;
@@ -16,6 +17,7 @@ public class PathLabsInfoServiceApplication {
 	}
 	
 	
+	@Bean
 	public CommandLineRunner runner() {
 		
 		return new CommandLineRunner() {
--- path-labs-info-service/src/main/java/com/example/demo/repo/PathLabRepository.java
+++ path-labs-info-service/src/main/java/com/example/demo/repo/PathLabRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.demo.entity.PathLab;
 
-
+@RepositoryRestResource(path = "labs")
 public interface PathLabRepository extends JpaRepository<PathLab, Integer> {
 
 }
