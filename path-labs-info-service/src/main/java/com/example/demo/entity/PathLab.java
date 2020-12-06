e800b309b9da0cc6e1b2a04df4c937416eb2fa9a
vatsank <vatsank@gmail.com>
2020-12-06 15:30:27 +0530

    Update

---
+++ path-labs-info-service/src/main/java/com/example/demo/entity/PathLab.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "practo_labs")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PathLab {
+
+	
+	private int id;
+	private String labName;
+	private String location;
+	private double rating;
+	
+}
---
+++ path-labs-info-service/src/main/java/com/example/demo/repo/PathLabRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.PathLab;
+
+
+public interface PathLabRepository extends JpaRepository<PathLab, Integer> {
+
+}
