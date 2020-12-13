b363e95e52bdb16b0ad49b036dde6dd5d2838f51
vatsank <vatsank@gmail.com>
2020-12-13 16:40:36 +0530

    Update

--- secured-service/src/main/java/com/example/demo/config/SecurityConfig.java
+++ secured-service/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
