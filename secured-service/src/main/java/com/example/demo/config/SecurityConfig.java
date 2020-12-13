8d9f1e94a42d39d48e585e294d7c3c619f6449e4
vatsank <vatsank@gmail.com>
2020-12-13 16:24:06 +0530

    Update

--- secured-service/src/main/java/com/example/demo/config/SecurityConfig.java
+++ secured-service/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,21 +3,29 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+
+	
+		auth.inMemoryAuthentication().withUser("india").password("{noop}india").roles("ADMIN")
+		     .and().withUser("nepal").password("{noop}nepal").roles("USER");
+		
+		
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+
+		 http.authorizeRequests().antMatchers("/api/v1/customers/**").authenticated()
+		              .and().httpBasic().and().csrf().disable();
+	
 	}
 
 	
