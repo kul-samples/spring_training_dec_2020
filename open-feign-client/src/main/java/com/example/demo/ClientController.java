327fe7e990e12f9ac0581dcef63bdeb730d3670f
vatsank <vatsank@gmail.com>
2020-12-13 14:51:01 +0530

    Update

--- open-feign-client/src/main/java/com/example/demo/ClientController.java
+++ open-feign-client/src/main/java/com/example/demo/ClientController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +12,10 @@ public class ClientController {
 	@Autowired
 	private FiegnLoadBalancedClient client;
 	
-	
+	@GetMapping(path = "/client/{hospitalName}")
+	public String getReviews(@PathVariable("hospitalName") String name) {
+		
+		return client.getReview(name);
+	}
 	
 }
