0b4964a26636d5c6bbcb64b739e636cf2c4584b6
vatsank <vatsank@gmail.com>
2020-12-13 09:58:45 +0530

    Update

--- lab-service/src/main/java/com/example/demo/LabServiceApplication.java
+++ lab-service/src/main/java/com/example/demo/LabServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 public class LabServiceApplication {
