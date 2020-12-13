6b41790df0a2ab45f8a26c8f4ed0ed01592ad318
vatsank <vatsank@gmail.com>
2020-12-13 16:55:18 +0530

    update

--- secured-service/src/main/java/com/example/demo/SecuredServiceApplication.java
+++ secured-service/src/main/java/com/example/demo/SecuredServiceApplication.java
@@ -3,8 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
 import com.example.demo.model.*;
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
+
 public class SecuredServiceApplication {
 
 	public static void main(String[] args) {
--- secured-service/src/main/java/com/example/demo/services/CustomerService.java
+++ secured-service/src/main/java/com/example/demo/services/CustomerService.java
@@ -2,8 +2,11 @@ package com.example.demo.services;
 import com.example.demo.model.*;
 import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,11 +21,15 @@ public class CustomerService {
 	@Autowired
 	private List<Customer> custList;
 	
+	
+	@Secured("ROLE_USER")
 	public Customer getCustomer(int id) {
 		
 		return ramesh;
 	}
 	
+	
+	@RolesAllowed({"ROLE_ADMIN"})
 	public List<Customer> getAllCustomers(){
 		
 		return this.custList;
