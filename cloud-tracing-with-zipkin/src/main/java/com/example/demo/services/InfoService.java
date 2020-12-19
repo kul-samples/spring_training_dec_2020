a07dbb7fcd85339da30b0bb7cc0608668654803b
vatsank <vatsank@gmail.com>
2020-12-19 16:48:50 +0530

    Update

--- cloud-tracing-with-zipkin/src/main/java/com/example/demo/services/InfoService.java
+++ cloud-tracing-with-zipkin/src/main/java/com/example/demo/services/InfoService.java
@@ -1,14 +1,55 @@
 package com.example.demo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.annotation.ContinueSpan;
+import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.stereotype.Service;
 
 import brave.Tracer;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class InfoService {
 
 	
 	@Autowired
 	private Tracer tracer;
+	
+	
+	public String firstOperation() {
+		
+		log.info("First Operation Started");
+		
+		return "Hello From First";
+		
+	}
+	
+	@NewSpan
+	public String secondOpeation() {
+		
+		String resp ="From Second";
+		log.info("Second Operation Called");
+		
+		try {
+			Thread.sleep(1000);
+			
+			resp = thirdOperations();
+		} catch (InterruptedException e) {
+			log.debug("Inside catch block");
+			e.printStackTrace();
+		}
+		
+		log.info("Finished SecondOperation");
+		
+		return resp;
+	}
+	
+	
+	@ContinueSpan
+	public String thirdOperations() {
+		
+		
+		return "Bye Bye";
+	}
 }
