f044a97c6a8886f0c1e7e7bb227505a195c2758e
vatsank <vatsank@gmail.com>
2020-12-19 11:35:03 +0530

    Update

--- secured-service-with-jdbc/src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ secured-service-with-jdbc/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -1,5 +1,49 @@
 package com.example.demo.config;
 
-public class SecurityConfiguration {
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		
+		auth.jdbcAuthentication().dataSource(dataSource);
+		
+	
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+		    .antMatchers("/api/v1/**")
+		      .authenticated()
+		        .and()
+		           .csrf()
+		               .disable()
+		                .httpBasic();
+	
+	}
+
+	// This Bean provides the CRUD Operations to work the user and authorities tables
+	@Bean
+	public JdbcUserDetailsManager  userDetailsManager() {
+		
+		return new JdbcUserDetailsManager(dataSource);
+	}
+	
 }
