b4c659c0389343170890b64b3f6921ac821e2444
vatsank <vatsank@gmail.com>
2020-12-19 10:47:47 +0530

    Update

--- secured-service-with-jdbc/src/main/java/com/example/demo/SecuredServiceWithJdbcApplication.java
+++ secured-service-with-jdbc/src/main/java/com/example/demo/SecuredServiceWithJdbcApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.model.Hospital;
 
 @SpringBootApplication
 public class SecuredServiceWithJdbcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecuredServiceWithJdbcApplication.class, args);
+	}
+
+	
+	@Bean
+	public Hospital malar() {
+		
+		return new Hospital(101,"Malar Hospital",100100);
+	}
+	
+	@Bean
+	public Hospital appollo() {
+		
+		return new Hospital(102,"Apollo Hospital",200100);
+		
 	}
 
 }
--- secured-service-with-jdbc/src/main/java/com/example/demo/controllers/HospitalController.java
+++ secured-service-with-jdbc/src/main/java/com/example/demo/controllers/HospitalController.java
@@ -1,5 +1,35 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Hospital;
+
+@RestController
 public class HospitalController {
 
+	
+	
+	@Autowired
+	private Hospital malar;
+	
+	@Autowired
+	private Hospital appollo;
+	
+	
+	
+	@GetMapping(path = "/api/v1/hospitals/{id}")
+	public Hospital getHospitalById(@PathVariable("id") int id) {
+		
+		Hospital obj = malar;
+		
+		 if(id==1) {
+			 obj = appollo;
+		 }
+		
+		 return obj;
+	}
+	
 }
